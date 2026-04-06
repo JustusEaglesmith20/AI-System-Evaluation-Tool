7425fcc932713f7a47783d6eea3215381fa5d267
Justus Eaglesmith <justuseaglesmith@gmail.com>
2026-04-05 18:24:53 -0700

    Refactor LLM input handling and add test data CSV

--- evaluator/llm_judge.py
+++ evaluator/llm_judge.py
@@ -6,64 +6,39 @@ import os
 from dotenv import load_dotenv
 
 import pandas as pd
-from typing import List
 
 def send_inputs_to_LLM(eval_input_data_path: str):
     """
     Function that takes a csv as an input & sends the input questions to a selected LLM
     URI/Endpoint & returns a csv of the responses.
     """
-    def _setup_test_config():
-        
-        load_dotenv()
-        api_key = os.getenv('OPENAI_APIKEY')
 
+    client = OpenAI()
 
-        test_prompt = PromptConfig.test_prompt
-        target_client = OpenAI(api_key=api_key)
-
-    def _send_question_to_model(target_client, test_prompt):
-        response = target_client.responses.create(
+    def _send_question_to_model(question) -> str:
+        response = client.responses.create(
             model="gpt-5.4-mini",
-            input= test_prompt
+            input= question
         )
-        return response
+        return response.output_text
         
-            
     data = pd.read_csv(rf"{eval_input_data_path}")
-    
-    # responses = List[str]
-    questions_and_responses = dict[str:str]
-    
-    _ = data['input'].map(lambda row: _send_question_to_model(row))
 
-    print(_)
+    answers:dict[str,str] = {}
+    question = ""
+    data
 
-    # for each row in df > wait 2 seconds > async send to LLM > append to respective key > write to csv
+    qa_dict = {q: _send_question_to_model(q) for q in data['input']}
 
+    return qa_dict
 
-def main():
-
+if __name__ == "__main__":
     load_dotenv()
     path = os.getenv("EVAL_INPUT_DATA_PATH")
 
     config = Config(path)
     config.setup_eval_config()
 
-    send_inputs_to_LLM(eval_input_data_path=path)
+    qa_dict = send_inputs_to_LLM(eval_input_data_path=path)
 
-    # api_key = os.getenv('OPENAI_APIKEY')
-    # load_dotenv()
-
-    # eval_prompt = PromptConfig.eval_prompt
-    # client = OpenAI(api_key=api_key)
-
-    # response = client.responses.create(
-    #     model="gpt-5.4-mini",
-    #     input= eval_prompt
-    # )
-
-    # print(response.output_text)
-
-if __name__ == "__main__":
-    main()
+    # Now I just need to have the second llm evaluate the resposnes.
