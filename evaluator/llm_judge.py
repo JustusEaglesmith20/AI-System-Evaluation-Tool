e83a628f73c91eb9564ee40af24ee0e370a94479
Justus Eaglesmith <justuseaglesmith@gmail.com>
2026-04-08 07:43:38 -0700

    Add output_results.csv and refactor LLM input handling to improve response storage

--- evaluator/llm_judge.py
+++ evaluator/llm_judge.py
@@ -12,9 +12,8 @@ def send_inputs_to_LLM_and_update_df(eval_input_data_path: str):
     Function that takes a csv as an input & sends the input questions to a selected LLM
     URI/Endpoint & returns a csv of the responses.
     """
-
     client = OpenAI()
-
+    
     def _send_question_to_model(question):
         response = client.responses.create(
             model="gpt-5.4-mini",
@@ -22,13 +21,15 @@ def send_inputs_to_LLM_and_update_df(eval_input_data_path: str):
         )
         return response.output_text
         
-    data = pd.read_csv(rf"{eval_input_data_path}")
+    data = pd.read_csv(eval_input_data_path)
     df = data.copy()
 
-    for row in df:
-        for q in df['input']: 
-            resp = _send_question_to_model(q)
-            df.loc[row,'target_llm_output'] = resp
+    target_llm_output = []
+    for q in df['input']:
+        resp = _send_question_to_model(q)
+        target_llm_output.append(resp)
+
+    df['target_llm_output'] = target_llm_output
 
     return df
 
@@ -40,12 +41,18 @@ def construct_data_model():
 
 if __name__ == "__main__":
     load_dotenv()
-    path = os.getenv("EVAL_INPUT_DATA_PATH")
+    input_path = os.getenv("EVAL_INPUT_DATA_PATH")
+    output_path = os.getenv("EVAL_OUTPUT_DATA_PATH")
 
-    config = Config(path)
+    config = Config(input_path)
     config.setup_eval_config()
 
     # TODO: Need to set up a data model for the required format of the CSV
-    test_df = send_inputs_to_LLM_and_update_df(eval_input_data_path=path)
-    print(test_df)
+    try:
+        test_df = send_inputs_to_LLM_and_update_df(eval_input_data_path=input_path)
+    except Exception as e:
+        print(f"Exception {e}: Failure when running send_inputs_to_LLM_and_update_df function.")
+
+    test_df.to_csv(output_path)
+    print (f"Results stored & writted to {output_path}")
     # Now I just need to have the second llm evaluate the resposnes.
