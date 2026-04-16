06568be064510da09568e0539273ae04d231008a
Justus Eaglesmith <justuseaglesmith@gmail.com>
2026-04-16 11:03:56 -0700

    Refactoring into interface -> Adapter -> Eval

--- evaluator/llm_judge.py
+++ evaluator/llm_judge.py
@@ -7,59 +7,101 @@ from dotenv import load_dotenv
 
 import pandas as pd
 
-def send_inputs_to_LLM_and_update_df(eval_input_data_path: str):
-    """
-    Function that takes a csv as an input & sends the input questions to a selected LLM
-    URI/Endpoint & returns a csv of the responses.
-    """
-    client = OpenAI()
+from abc import ABC, abstractmethod
+from typing import List, Dict
+
+'''
+TODO:
+Finish refactoring into interface -> adapter -> eval design
+write code to load in csv into records struct
+finish send_question_to_llm method refactor
+
+'''
+
+class TargetAIClient(ABC):    
+    # TODO: Need to convert to async
+    @abstractmethod
+    def send_question_to_llm(records) -> str:
+        ...
+
+
+class OpenAIAdapter(TargetAIClient):
+    def __init__(self, api_key: str, model:str = "gpt-5.4-mini"):
+        self.api_key = api_key
+        self.model = model
     
-    def _send_question_to_model(question):
-        response = client.responses.create(
-            model="gpt-5.4-mini",
-            input= question
-        )
-        return response.output_text
+    def load_records() -> List[Dict]:
+        pass
+
+
+    def send_question_to_llm(records):
+        openai_client = OpenAI(api_key=api_key)
+
+        response = openai_client.responses.create(
+                    model= model,
+                    input= question
+                )
+            
+    def send_question_to_llm(records):
+        for q in records['question']:
+
+            return response.output_text
+            
+         
         
-    data = pd.read_csv(eval_input_data_path)
-    df = data.copy()
 
-    target_llm_output = []
-    for q in df['input']:
-        resp = _send_question_to_model(q)
-        target_llm_output.append(resp)
 
-    df['target_llm_output'] = target_llm_output
 
-    return df
 
-def llm_judge_evaluator(eval_output_data_path: str):
-    """
-    Function instantiates to update output dataset with judge analysis
-    """
-    judgeclient = OpenAI()
+class Evaluator(target_llm_client, llm_judge_client):
     
-    results = pd.read_csv(eval_output_data_path)
+    def __init__(self, target_llm_client, llm_judge_client):
+        self.target_llm_client = target_llm_client
+        self.llm_judge_client = llm_judge_client
 
-    def _evaluate_target_llm_qs(q):
-        response = judgeclient.responses.create(
-            model="gpt-5.4-mini",
-            input= q
-        )
-        return response.output_text
+    def send_inputs_to_LLM_and_update_df(self, eval_input_data_path: str):
+        """
+        Function that takes a csv as an input & sends the input questions to a selected LLM
+        URI/Endpoint & returns a csv of the responses.
+        """
+        client = OpenAI()
         
 
-    eval = []
-    for q in results['target_llm_output']:
-        resp = _evaluate_target_llm_qs(q)
-        eval.append(resp)
+            
+        data = pd.read_csv(eval_input_data_path)
+        df = data.copy()
+        target_llm_output = []
+        for q in df['input']:
+            resp = _send_question_to_model(q)
+            target_llm_output.append(resp)
 
-    results['evaluation'] = eval
+        df['target_llm_output'] = target_llm_output
+        return df
 
-    return results
+    def llm_judge_evaluator(self, eval_output_data_path: str):
+        """
+        Function instantiates to update output dataset with judge analysis
+        """
+        judgeclient = OpenAI()        
+        results = pd.read_csv(eval_output_data_path)
 
-def construct_data_model():
-    pass
+        def _evaluate_target_llm_qs(self, q):
+            response = judgeclient.responses.create(
+                model="gpt-5.4-mini",
+                input= q
+            )
+            return response.output_text
+            
+        eval = []
+        for q in results['target_llm_output']:
+            resp = _evaluate_target_llm_qs(q)
+            eval.append(resp)
+
+        results['evaluation'] = eval
+        return results
+
+    def construct_data_model():
+        pass
 
 if __name__ == "__main__":
     load_dotenv()
